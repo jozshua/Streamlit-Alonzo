92257e1263ae5e6d7d884c3648a33f3f0b66f0fc
jozshua <112044210+jozshua@users.noreply.github.com>
2023-05-20 20:12:42 +0800

    Update streamlit_alonzo.py

--- streamlit_alonzo.py
+++ streamlit_alonzo.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-  model=tf.keras.models.load_model('/content/drive/MyDrive/Colab Notebooks/Multi-class Weather Dataset Model/best_model.h5')
+  model=tf.keras.models.load_model('best_model.h5')
   return model
 model=load_model()
 st.write("""
