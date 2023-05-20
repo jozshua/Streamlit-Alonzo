bd4b9c1aa46d9d081a69d1a9b536f3da33a4e15c
jozshua <112044210+jozshua@users.noreply.github.com>
2023-05-20 20:00:18 +0800

    Update streamlit_alonzo.py

--- streamlit_alonzo.py
+++ streamlit_alonzo.py
@@ -7,11 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1-COkGSJu3mF8tV6i4kPdUVn_eSLpxGf_
 """
 
-from google.colab import drive
-drive.mount('/content/drive')
-
-!pip install streamlit
-
 import streamlit as st
 import tensorflow as tf
 
